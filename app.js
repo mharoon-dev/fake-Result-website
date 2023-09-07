ea209358a972904ccaf32121021a70bb69028813
haroon124 <agar3d@gmail.com>
2023-09-07 17:45:00 +0500

    fake web

--- app.js
+++ app.js
@@ -3,16 +3,16 @@ const input = document.getElementById("input")
 
 function searchBtn() {
     if (input.value == "202848") {
-        window.location = "./result page/results/result 1.html"
+        window.location = "./results/result 1.html"
     } else if (input.value == "202854" ) {
-        window.location = "./result page/results/result 2.html"
+        window.location = "./results/result 2.html"
     } else if (input.value == "202849" ) {
-        window.location = "./result page/results/result 3.html"
+        window.location = "./results/result 3.html"
     } else if (input.value == "253573" ) {
-        window.location = "./result page/results/result 4.html"
-    } else if (input.value == "061939" ) {
-        window.location = "./result page/results/result 5.html"
-    } else {
+        window.location = "./results/result 4.html"
+    } //else if (input.value == "061939" ) {
+        //window.location = "./results/result 5.html"}
+     else {
         alert("Write the correct roll number")
     }
 }
