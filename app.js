b832d49f6e6176a4e3fe316e195f69656ca3c323
haroon124 <agar3d@gmail.com>
2023-09-07 17:51:00 +0500

    fake web

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const input = document.getElementById("input")
 function searchBtn() {
     if (input.value == "202848") {
         window.location = "./results/result 1.html"
-    } else if (input.value == "202854" ) {
-        window.location = "./results/result 2.html"
     } else if (input.value == "202849" ) {
+        window.location = "./results/result 2.html"
+    } else if (input.value == "202854" ) {
         window.location = "./results/result 3.html"
     } else if (input.value == "253573" ) {
         window.location = "./results/result 4.html"
